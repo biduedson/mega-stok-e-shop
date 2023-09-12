6e46d0d298b67c47ed331c323295abcef1430851
biduedson <112134265+biduedson@users.noreply.github.com>
2023-09-12 15:38:47 -0300

    feito footer,home e products

---
+++ app/product/[productId]/page.tsx
@@ -0,0 +1,20 @@
+import Container from "@/app/components/Container";
+import ProductDetails from "./ProductDetails";
+import { product } from "@/utils/product";
+
+interface IPrams {
+  productId?: string;
+}
+
+const Product = ({ params }: { params: IPrams }) => {
+  console.log("params", params);
+  return (
+    <div className="p-8">
+      <Container>
+        <ProductDetails product={product} />
+      </Container>
+    </div>
+  );
+};
+
+export default Product;
